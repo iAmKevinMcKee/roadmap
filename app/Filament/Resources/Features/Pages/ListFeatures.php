686d5a60e41cad90e3e0c4ac0d05803aa1f1d07d
Kevin McKee <mckee.kevin@gmail.com>
2025-11-26 17:59:16 -0600

    strict authorization

--- app/Filament/Resources/Features/Pages/ListFeatures.php
+++ app/Filament/Resources/Features/Pages/ListFeatures.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Resources\Features\Pages;
 
 use App\Filament\Resources\Features\FeatureResource;
+use App\Models\Feature;
+use Filament\Actions\Action;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Support\Facades\Gate;
 
 class ListFeatures extends ListRecords
 {
@@ -13,6 +16,10 @@ class ListFeatures extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
+            Action::make('test')->action(function () {
+                dd('Test action executed');
+            })
+                ->authorize(Gate::allows('test', Feature::class)),
             CreateAction::make(),
         ];
     }
