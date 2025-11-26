500c0ea2daabe314d0e7df8363da637777d2e9c6
Kevin McKee <mckee.kevin@gmail.com>
2025-11-26 17:28:11 -0600

    service provider

--- app/Filament/Pages/EditProfile.php
+++ app/Filament/Pages/EditProfile.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Pages;
 
 use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 
 class EditProfile extends \Filament\Auth\Pages\EditProfile
@@ -11,18 +12,17 @@ class EditProfile extends \Filament\Auth\Pages\EditProfile
     {
         return $schema
             ->components([
-                $this->getNameFormComponent(),
-                $this->getEmailFormComponent(),
-                TextInput::make('phone')
-                    ->tel()
-                    ->minLength(14)
-                    ->maxLength(14)
-                    ->prefixIcon('heroicon-o-phone')
-                    ->mask('(999) 999-9999')
-                    ->required(),
-                $this->getPasswordFormComponent(),
-                $this->getPasswordConfirmationFormComponent(),
-                $this->getCurrentPasswordFormComponent(),
+                Section::make('Profile Information')
+                    ->schema([
+                        $this->getNameFormComponent(),
+                        $this->getEmailFormComponent(),
+                        TextInput::make('phone')
+                            ->phone()
+                            ->required(),
+                        $this->getPasswordFormComponent(),
+                        $this->getPasswordConfirmationFormComponent(),
+                        $this->getCurrentPasswordFormComponent(),
+                    ]),
             ]);
     }
 }
