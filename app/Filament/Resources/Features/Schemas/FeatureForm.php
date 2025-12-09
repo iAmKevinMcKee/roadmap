29a9a757a125ca23e9e3d8525dfa52583e616ad1
Kevin McKee <mckee.kevin@gmail.com>
2025-12-08 21:49:45 -0600

    lesson-9

--- app/Filament/Resources/Features/Schemas/FeatureForm.php
+++ app/Filament/Resources/Features/Schemas/FeatureForm.php
@@ -12,6 +12,8 @@ use Filament\Forms\Components\Slider;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
 use Filament\Forms\Components\ToggleButtons;
+use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\Tabs;
 use Filament\Schemas\Components\Utilities\Get;
 use Filament\Schemas\Components\Utilities\Set;
 use Filament\Schemas\Schema;
@@ -24,72 +26,112 @@ class FeatureForm
         return $schema
             ->columns(3)
             ->components([
-                TextInput::make('name')
-                    ->required(),
-                Select::make('status')
-                    ->options(FeatureStatus::class)
-                    ->enum(FeatureStatus::class)
-                    ->searchable()
-                    ->required()
-                    ->default(FeatureStatus::Proposed),
-                DatePicker::make('target_delivery_date')
-                    ->rules([
-                        function (Get $get) {
-                            return Rule::requiredIf($get('status') === FeatureStatus::Planned || $get('status') === FeatureStatus::InProgress);
-                        }
-                    ])
-                    ->visibleJs(<<<'JS'
-                        $get('status') === 'Planned' || $get('status') === 'In Progress'
-                    JS
-                    ),
-                ToggleButtons::make('type')
-                    ->hiddenLabel()
-                    ->options(FeatureType::class)
-                    ->enum(FeatureType::class)
-                    ->inline()
-                    ->required()
-                    ->default(FeatureType::Feature),
-                Slider::make('priority')
-                    ->required()
-                    ->minValue(1)
-                    ->maxValue(10)
-                    ->pips(Slider\Enums\PipsMode::Steps)
-                    ->step(1)
-                    ->fillTrack()
-                    ->default(0),
-                RichEditor::make('description')
-                    ->toolbarButtons([
-                        ['bold', 'italic', 'underline', 'strike', 'link'],
-                        ['h2', 'h3', 'alignStart', 'alignCenter', 'alignEnd'],
-                        ['blockquote', 'codeBlock', 'bulletList', 'orderedList'],
-                    ])
-                    ->required()
-                    ->columnSpanFull(),
-                TextInput::make('effort_in_days')
-                    ->required()
-                    ->numeric()
-                    ->afterStateUpdatedJs(<<<'JS'
+                Tabs::make('Feature Tabs')
+                    ->columnSpanFull()
+                    ->tabs([
+                        Tabs\Tab::make('General')
+                            ->columns(3)
+                            ->schema([
+                                TextInput::make('name')
+                                    ->required(),
+                                Select::make('status')
+                                    ->options(FeatureStatus::class)
+                                    ->enum(FeatureStatus::class)
+                                    ->searchable()
+                                    ->required()
+                                    ->default(FeatureStatus::Proposed),
+                                Slider::make('priority')
+                                    ->required()
+                                    ->extraFieldWrapperAttributes([
+                                        'class' => 'pl-3'
+                                    ])
+                                    ->minValue(1)
+                                    ->maxValue(10)
+                                    ->pips(Slider\Enums\PipsMode::Steps)
+                                    ->step(1)
+                                    ->fillTrack()
+                                    ->default(0),
+                                ToggleButtons::make('type')
+                                    ->hiddenLabel()
+                                    ->options(FeatureType::class)
+                                    ->enum(FeatureType::class)
+                                    ->inline()
+                                    ->required()
+                                    ->default(FeatureType::Feature),
+                                DatePicker::make('target_delivery_date')
+                                    ->rules([
+                                        function (Get $get) {
+                                            return Rule::requiredIf($get('status') === FeatureStatus::Planned || $get('status') === FeatureStatus::InProgress);
+                                        }
+                                    ])
+                                    ->visibleJs(<<<'JS'
+                                $get('status') === 'Planned' || $get('status') === 'In Progress'
+                            JS
+                                    ),
+                                DateTimePicker::make('delivered_at')
+                                    ->visibleJs(<<<'JS'
+                                $get('status') === 'Completed'
+                            JS
+                                    ),
+                                RichEditor::make('description')
+                                    ->extraInputAttributes([
+                                        'style' => 'min-height: 150px;',
+                                    ])
+                                    ->toolbarButtons([
+                                        ['bold', 'italic', 'underline', 'strike', 'link'],
+                                        ['h2', 'h3', 'alignStart', 'alignCenter', 'alignEnd'],
+                                        ['blockquote', 'codeBlock', 'bulletList', 'orderedList'],
+                                    ])
+                                    ->required()
+                                    ->columnSpanFull(),
+                            ]),
+                        Tabs\Tab::make('Effort and Cost')
+                            ->columns(2)
+                            ->schema([
+                                TextInput::make('effort_in_days')
+                                    ->required()
+                                    ->numeric()
+                                    ->afterStateUpdatedJs(<<<'JS'
                         const isHighCost = $get('is_high_cost');
                         const effort = $state;
                         const costPerDay = isHighCost ? 1500 : 1000;
                         $set('cost', effort * costPerDay);
-                    JS)
-                    ->default(0),
-                TextInput::make('cost')
-                    ->required()
-                    ->numeric()
-                    ->default(0.0)
-                    ->prefix('$'),
-                Toggle::make('is_high_cost')
-                    ->label('Is High Cost')
-                    ->dehydrated(false)
-                    ->afterStateUpdatedJs(<<<'JS'
+                    JS
+                                    ),
+                                TextInput::make('cost')
+                                    ->required()
+                                    ->numeric()
+                                    ->default(0.0)
+                                    ->prefix('$'),
+                                Toggle::make('is_high_cost')
+                                    ->label('Is High Cost')
+                                    ->dehydrated(false)
+                                    ->afterStateUpdatedJs(<<<'JS'
                         const isHighCost = $state;
                         const effort = $get('effort_in_days');
                         const costPerDay = isHighCost ? 1500 : 1000;
                         $set('cost', effort * costPerDay);
-                        JS),
-                DateTimePicker::make('delivered_at'),
+                        JS
+                                    ),
+                            ]),
+
+//                        Section::make('Feature Details')
+//                            ->collapsible()
+//                            ->description('Provide the details for the feature.')
+//                            ->columns(3)
+//                            ->columnSpanFull()
+//                            ->schema([
+//
+//                            ]),
+//                        Section::make('Effort and Cost')
+//                            ->description('Estimate the effort and cost for the feature.')
+//                            ->collapsible()
+//                            ->columns(2)
+//                            ->columnSpanFull()
+//                            ->schema([
+//
+//                            ]),
+                    ]),
             ]);
     }
 }
