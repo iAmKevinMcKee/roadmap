aa8bd2c5c68b7f9a33ae59adee85ed14f3c05468
Kevin McKee <mckee.kevin@gmail.com>
2025-12-15 22:21:07 -0600

    lesson-7

--- app/Filament/Resources/Features/Schemas/FeatureForm.php
+++ app/Filament/Resources/Features/Schemas/FeatureForm.php
@@ -10,24 +10,38 @@ use Filament\Forms\Components\RichEditor;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Slider;
 use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\ToggleButtons;
+use Filament\Schemas\Components\Utilities\Get;
 use Filament\Schemas\Schema;
+use Illuminate\Validation\Rule;
 
 class FeatureForm
 {
     public static function configure(Schema $schema): Schema
     {
         return $schema
+            ->columns(3)
             ->components([
                 TextInput::make('name')
                     ->required(),
                 Select::make('status')
+                    ->live()
                     ->options(FeatureStatus::class)
                     ->enum(FeatureStatus::class)
                     ->searchable()
                     ->required()
                     ->default(FeatureStatus::Proposed),
+                DatePicker::make('target_delivery_date')
+                    ->rules([
+                        function (Get $get) {
+                            return Rule::requiredIf($get('status') === FeatureStatus::Planned
+                                || $get('status') === FeatureStatus::InProgress);
+                        }
+                    ])
+                    ->visibleJs(<<<'JS'
+                        $get('status') === 'Planned' || $get('status') === 'In Progress'
+                    JS
+                    ),
                 ToggleButtons::make('type')
                     ->hiddenLabel()
                     ->options(FeatureType::class)
@@ -60,7 +74,6 @@ class FeatureForm
                     ->numeric()
                     ->default(0.0)
                     ->prefix('$'),
-                DatePicker::make('target_delivery_date'),
                 DateTimePicker::make('delivered_at'),
             ]);
     }
